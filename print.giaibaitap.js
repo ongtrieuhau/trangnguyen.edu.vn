7f62021d01d0726dfea6d459a53339ae5f88b372
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-07 12:32:33 +0700

    Update print.giaibaitap.js

--- print.giaibaitap.js
+++ print.giaibaitap.js
@@ -1,13 +1,12 @@
+var removeClasses = ["list-item-chapter", "title-answer ng-binding", "title-answer2 ng-scope", "footer", "header", "menu", "navigator"];
+var paddingClasses = ["title-box-contest ng-binding", "answer-box", "detail-box-contest", "box-contest"];
 for (let k = 0; k <= 5; k++) {
-   let removeClasses = ["list-item-chapter", "title-answer ng-binding", "title-answer2 ng-scope", "footer", "header", "menu", "navigator"];
    for (let i = 0; i < removeClasses.length; i++) {
       let els = document.getElementsByClassName(removeClasses[i]);
       for (let j = 0; j < els.length; j++) {
          els[j].remove();
       }
    }
-
-   let paddingClasses = ["title-box-contest ng-binding", "answer-box", "detail-box-contest", "box-contest"];
    for (let i = 0; i < paddingClasses.length; i++) {
       let els = document.getElementsByClassName(paddingClasses[i]);
       for (let j = 0; j < els.length; j++) {
